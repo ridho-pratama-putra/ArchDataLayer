d7e4d6e71737f68ae97cb6b0a89a09cebc4b6ae7
ridho <ridhopratamavd@gmail.com>
2024-04-19 06:41:23 +0700

    save to db success

--- app/src/main/java/com/example/archdatalayer/sleepTracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/archdatalayer/sleepTracker/SleepTrackerViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.archdatalayer.sleepTracker
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.archdatalayer.database.SleepDatabaseDao
 import com.example.archdatalayer.database.SleepNight
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class SleepTrackerViewModel(
@@ -14,13 +19,46 @@ class SleepTrackerViewModel(
 
     private var tonight = MutableLiveData<SleepNight?>()
 
-    //    encapsulate mutable livedata
+    // encapsulate mutable livedata
     private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
     val navigateToSleepQuality: LiveData<SleepNight> get() = _navigateToSleepQuality
 
+    // to manage coroutine (seperti cancel all coroutine saat viewmodel nolonger use dan destroyed)
+    private var viewModelJob = Job()
+
+    // scope determine what thread the coroutine will run on and its need to know about the job
+    // this means coroutine launch in the ui scope will run on main thread
+    private val ioScope = CoroutineScope(Dispatchers.IO + viewModelJob)
+    private val mainScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
     fun onButtonStartPressed() {
         Timber.i("onButtonStartPressed")
-        tonight.value = SleepNight()
+        ioScope.launch {
+            delay(5000)
+
+            Timber.i("onstarttracking launch")
+            val newNight = SleepNight()
+
+            database.save(newNight)
+        }
+
+        // Cannot invoke setValue on a background thread so I use main thread
+        mainScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        Timber.i("getTonightFromDatabase")
+        return withContext(Dispatchers.IO) {
+            Timber.i("getTonightFromDatabase with context")
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
     }
 
     fun onButtonStopPressed() {
