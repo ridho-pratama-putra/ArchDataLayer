7aa90f92a125193648500e22d96ec5c6792e0e00
ridho <ridhopratamavd@gmail.com>
2024-04-24 13:26:02 +0700

    refactor to comply separation of concerns

--- app/src/main/java/com/example/archdatalayer/sleepTracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/archdatalayer/sleepTracker/SleepNightAdapter.kt
@@ -1,16 +1,12 @@
 package com.example.archdatalayer.sleepTracker
 
-import android.graphics.Color
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.archdatalayer.R
-import com.example.archdatalayer.convertDurationToFormatted
-import com.example.archdatalayer.convertNumericQualityToString
 import com.example.archdatalayer.database.SleepNight
-import timber.log.Timber
 
+/*
+* this class responsible to adapt data tobe used by recycle view
+ */
 class SleepNightAdapter: RecyclerView.Adapter<ViewHolder>() {
     var data = listOf<SleepNight>()
         set(value) {
@@ -19,10 +15,7 @@ class SleepNightAdapter: RecyclerView.Adapter<ViewHolder>() {
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false) as View
-        Timber.i("onCreateViewHolder $this")
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int {
@@ -35,23 +28,8 @@ class SleepNightAdapter: RecyclerView.Adapter<ViewHolder>() {
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        if (item.sleepQuality <= 1) {
-            holder.sleepDuration.setTextColor(Color.RED)
-        } else {
-            holder.sleepDuration.setTextColor(Color.BLACK)
-        }
         val resources = holder.itemView.context.resources
-        holder.sleepQuality.text = convertNumericQualityToString(item.sleepQuality, resources)
-        holder.sleepDuration.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, resources)
-        holder.sleepIcon.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(item, holder, resources)
         // Timber.i("onBindViewHolder $this $holder")
     }
 
--- app/src/main/java/com/example/archdatalayer/sleepTracker/ViewHolder.kt
+++ app/src/main/java/com/example/archdatalayer/sleepTracker/ViewHolder.kt
@@ -1,19 +1,63 @@
 package com.example.archdatalayer.sleepTracker
 
+import android.content.res.Resources
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.archdatalayer.R
+import com.example.archdatalayer.convertDurationToFormatted
+import com.example.archdatalayer.convertNumericQualityToString
+import com.example.archdatalayer.database.SleepNight
+import timber.log.Timber
 
 /**
  * ViewHolder that holds a single [TextView].
  *
  * A ViewHolder holds a view for the [RecyclerView] as well as providing additional information
  * to the RecyclerView such as where on the screen it was last drawn during scrolling.
+ * manage the view like which xml layout, match sleep icon, do colouring
  */
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val sleepDuration: TextView = itemView.findViewById(R.id.sleep_duration_string)
     val sleepQuality: TextView = itemView.findViewById(R.id.sleep_quality_string)
     val sleepIcon: ImageView = itemView.findViewById(R.id.sleep_quality_icon)
+
+     fun bind(
+        item: SleepNight,
+        holder: ViewHolder,
+        resources: Resources
+    ) {
+        if (item.sleepQuality <= 1) {
+            holder.sleepDuration.setTextColor(Color.RED)
+        } else {
+            holder.sleepDuration.setTextColor(Color.BLACK)
+        }
+        holder.sleepQuality.text = convertNumericQualityToString(item.sleepQuality, resources)
+        holder.sleepDuration.text =
+            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, resources)
+        holder.sleepIcon.setImageResource(
+            when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
+    }
+
+    companion object {
+        fun from (parent: ViewGroup): ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false) as View
+            Timber.i("onCreateViewHolder $this")
+            return ViewHolder(view)
+        }
+    }
 }
