936c250039248b84dc506408f4a0cda732cb37f6
ridho <ridhopratamavd@gmail.com>
2024-04-25 19:14:11 +0700

    use binding adapter

---
+++ app/src/main/java/com/example/archdatalayer/sleepTracker/BindingUtils.kt
@@ -0,0 +1,41 @@
+package com.example.archdatalayer.sleepTracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.archdatalayer.R
+import com.example.archdatalayer.convertDurationToFormatted
+import com.example.archdatalayer.convertNumericQualityToString
+import com.example.archdatalayer.database.SleepNight
+
+/**
+ * adapt application data into something dataBinding can display on the screen
+ */
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    item.let {
+        text = convertDurationToFormatted(it.startTimeMilli, it.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    item.let {
+        text = convertNumericQualityToString(it.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    item.let {
+        setImageResource(when(item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
--- app/src/main/java/com/example/archdatalayer/sleepTracker/ViewHolder.kt
+++ app/src/main/java/com/example/archdatalayer/sleepTracker/ViewHolder.kt
@@ -1,14 +1,10 @@
 package com.example.archdatalayer.sleepTracker
 
 import android.content.res.Resources
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.archdatalayer.R
-import com.example.archdatalayer.convertDurationToFormatted
-import com.example.archdatalayer.convertNumericQualityToString
 import com.example.archdatalayer.database.SleepNight
 import com.example.archdatalayer.databinding.ListItemSleepNightBinding
 import timber.log.Timber
@@ -25,24 +21,13 @@ class ViewHolder private constructor(private val binding: ListItemSleepNightBind
         item: SleepNight,
         resources: Resources
     ) {
-        if (item.sleepQuality <= 1) {
-            binding.sleepDurationString.setTextColor(Color.RED)
-        } else {
-            binding.sleepDurationString.setTextColor(Color.BLACK)
-        }
-        binding.sleepQualityString.text = convertNumericQualityToString(item.sleepQuality, resources)
-        binding.sleepDurationString.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, resources)
-        binding.sleepQualityIcon.setImageResource(
-            when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            }
-        )
+         binding.sleepNight = item
+
+         /**
+          * memastikan bahwa semua perubahan data pada setiap item di RecyclerView telah diterapkan ke tampilan
+          * sebelum item tersebut ditampilkan kepada pengguna.
+          */
+         binding.executePendingBindings()
     }
 
     companion object {
